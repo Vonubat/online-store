ffdb868ed2aa8eb41eb160e9fd0a296305af1b96
Egor Tabunov <Vonubat@gmail.com>
2022-07-08 16:08:48 +0300

    feat: create types/index.ts for describing type system

---
+++ online-store/src/components/types/index.ts
@@ -0,0 +1,12 @@
+export interface IGoods {
+  brand: string;
+  phone_name: string;
+  slug: string;
+  popular: boolean;
+  quantity: number;
+  color: string;
+  price: number;
+  camera: number;
+  year: number;
+  detail: string;
+}
