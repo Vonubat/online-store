5d225c6afa484fa5218922e9292cb9c7901efd2a
Egor Tabunov <Vonubat@gmail.com>
2022-07-09 19:02:01 +0300

    feat: create basic html&css structure

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,4 +1,8 @@
 import './styles/normalize.css';
 import './styles/main.scss';
-// import 'bootstrap';
+import 'bootstrap';
 // import * as bootstrap from 'bootstrap';
+
+// test for btn
+const btn = document.getElementById('button-addon1');
+btn?.addEventListener('mouseleave', btn.blur);
